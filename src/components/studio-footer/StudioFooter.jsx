097601d04b5cfc43ba1e9631d7fab2cce00e5cd9
Braden MacDonald <braden@opencraft.com>
2024-04-23 11:26:10 -0700

    chore: modernize this a bit

--- src/components/studio-footer/StudioFooter.jsx
+++ src/components/studio-footer/StudioFooter.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from 'react';
-import _ from 'lodash';
 import { intlShape, injectIntl, FormattedMessage } from '@edx/frontend-platform/i18n';
 import { ensureConfig } from '@edx/frontend-platform';
 import { AppContext } from '@edx/frontend-platform/react';
@@ -24,6 +23,8 @@ ensureConfig([
   'STUDIO_BASE_URL',
   'ENABLE_ACCESSIBILITY_PAGE',
 ], 'Studio Footer component');
+
+const isEmpty = (obj) => !Boolean(obj);
 
 const StudioFooter = ({
   // injected
@@ -79,7 +80,7 @@ const StudioFooter = ({
               >
                 <FormattedMessage {...messages.studioXButtonLabel} />
               </Button>
-              {!_.isEmpty(config.SUPPORT_EMAIL) && (
+              {!isEmpty(config.SUPPORT_EMAIL) && (
                 <Button
                   as="a"
                   href={`mailto:${config.SUPPORT_EMAIL}`}
@@ -96,11 +97,11 @@ const StudioFooter = ({
         <ActionRow className="pt-3 m-0 x-small">
           © {new Date().getFullYear()} <Hyperlink destination={config.MARKETING_SITE_BASE_URL} target="_blank" className="ml-2">{config.SITE_NAME}</Hyperlink>
           <ActionRow.Spacer />
-          {!_.isEmpty(config.TERMS_OF_SERVICE_URL) && (
+          {!isEmpty(config.TERMS_OF_SERVICE_URL) && (
             <Hyperlink destination={config.TERMS_OF_SERVICE_URL} data-testid="termsOfService">
               {intl.formatMessage(messages.termsOfServiceLinkLabel)}
             </Hyperlink>
-          )}{!_.isEmpty(config.PRIVACY_POLICY_URL) && (
+          )}{!isEmpty(config.PRIVACY_POLICY_URL) && (
             <Hyperlink destination={config.PRIVACY_POLICY_URL} data-testid="privacyPolicy">
               {intl.formatMessage(messages.privacyPolicyLinkLabel)}
             </Hyperlink>
